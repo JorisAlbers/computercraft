f94cec21a27c7f1f1ac35276c1f3814e19ac61f5
JorisAlbers <joris_albers@hotmail.com>
2023-02-26 20:27:40 +0100

    use github instead of pastebin

--- download_pastebin_on_startup.lua
+++ download_pastebin_on_startup.lua
@@ -1,6 +1,8 @@
-pastebin_link = ""
+git_location = ""
 file_name = "program.lua"
 
+git_url = "https://raw.githubusercontent.com/JorisAlbers/computercraft/main/"
+
 shell.execute("rm",file_name)
-shell.execute("pastebin","get",pastebin_link,file_name)
+shell.execute("wget", git_url..git_location, file_name)
 shell.execute("shell",file_name)
