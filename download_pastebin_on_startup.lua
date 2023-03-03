c32dc78cd257e3a06fff9797bb3e6551155b880c
JorisAlbers <joris_albers@hotmail.com>
2023-03-03 19:55:57 +0100

    added a sleep to prevent the server from crashing

--- download_pastebin_on_startup.lua
+++ download_pastebin_on_startup.lua
@@ -14,6 +14,7 @@ end
 
 local git_url = "https://raw.githubusercontent.com/JorisAlbers/computercraft/main/"
 
+sleep(1) -- to prevent server startup bug
 shell.execute("rm",file_name)
 shell.execute("wget", git_url..git_location, file_name)
 shell.execute("shell",file_name)
