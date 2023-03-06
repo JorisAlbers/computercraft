73ba5b56e3c38d40806a27fc95d06024c0f2f8cd
JorisAlbers <joris_albers@hotmail.com>
2023-03-06 22:02:32 +0100

    Use the correct github page for downloading the settings

--- elevator/console.lua
+++ elevator/console.lua
@@ -1,7 +1,7 @@
 -- elevator console
 
 -- load libraries
-local git_url = "https://raw.githubusercontent.com/JorisAlbers/computercraft/main/download_pastebin_on_startup.lua"
+local git_url = "https://raw.githubusercontent.com/JorisAlbers/computercraft/main/settings.lua"
 shell.run("rm","settings_manager.lua")
 shell.run("wget",git_url,"settings_manager.lua")
 require "settings_manager"
